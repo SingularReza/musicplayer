134b497a19fbaf5d7aca8747a15c2db474f66bb9
SingularReza <Chandandharana@gmail.comm>
2018-03-24 11:35:23 +0530

    2.4 SideNav stylized

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,10 @@
 $('#menu_button').click(function(){
 	if($('#options').width()===0)
 	{
-		$('#options').css('width', '20em');
+		$('#options').css('width', '400');
 	}
 
-	if($('#options').width()===320)
+	if($('#options').width()===400)
 	{
 		$('#options').css('width', '0');
 	}
