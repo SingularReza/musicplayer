876b06cd66924b652dcbd82d35439f2fbae82c96
SingularReza <Chandandharana@gmail.comm>
2018-03-24 12:06:35 +0530

    substituted PNG with SVG

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,10 +2,12 @@ $('#menu_button').click(function(){
 	if($('#options').width()===0)
 	{
 		$('#options').css('width', '400');
+		$('body').css('background', 'rgba(65, 65, 65, 0.9)');
 	}
 
 	if($('#options').width()===400)
 	{
 		$('#options').css('width', '0');
+		$('body').css('background', 'rgba(255, 255, 255, 1)');
 	}
 });
