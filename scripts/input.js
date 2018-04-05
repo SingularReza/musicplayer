d182b85b958e7580b0d85881c249e5950f2611fe
SingularReza <Chandandharana@gmail.comm>
2018-04-05 14:34:42 +0530

    reverting back

--- scripts/input.js
+++ scripts/input.js
@@ -2,7 +2,7 @@ $('input').change(function(){
 	var Files = this.files;
 
 	//for(var i=0; i<Files.length-1; i++){
-		$('audio').append("<source src=\""+Files[1].name+"\" type=\""+Files[1]+"\">");	
+		$('#albums').append("<div>"+Files[1].name+"</div>");	
 	//}
 	
 });
