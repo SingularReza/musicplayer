2a6868d38efc2c28f6c80cdfb8991358349884c0
SingularReza <Chandandharana@gmail.comm>
2018-04-18 01:39:29 +0530

    audio metadata extracts now

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,13 +1,32 @@
-$('#menu_button').click(function(){
-	if($('#options').width()===0)
+jQuery('#menu_button').click(function(){
+	if(jQuery('#options').width()===0)
 	{
-		$('#options').css('width', '400');
-		$('body').css('background', 'rgba(65, 65, 65, 0.9)');
+		jQuery('#options').css('width', '400');
+		jQuery('body').css('background', 'rgba(65, 65, 65, 0.9)');
+		jQuery('#info').css('width', window.innerWidth-400);
 	}
 
-	if($('#options').width()===400)
+	if(jQuery('#options').width()===400)
 	{
-		$('#options').css('width', '0');
-		$('body').css('background', 'rgba(255, 255, 255, 1)');
+		jQuery('#options').css('width', '0');
+		jQuery('body').css('background', 'rgba(255, 255, 255, 0.9)');
+		jQuery('#info').css('width', '0');
 	}
 });
+
+function check(){
+
+	jQuery('#now_playing').css('height', window.innerHeight);
+	jQuery('body').css('background', 'rgba(65, 65, 65, 0.9)');
+	jQuery('#go_back').css('visibility', 'visible');
+}
+
+jQuery('#go_back').click( function(){
+	if(jQuery('#now_playing').height()===window.innerHeight)
+	{
+		jQuery('#now_playing').css('height', '80');
+		jQuery('body').css('background', 'rgba(255, 255, 255, 0)');
+		jQuery('#go_back').css('visibility', 'hidden')
+	}
+});
+
